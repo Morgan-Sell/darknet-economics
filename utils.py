0b13b588b1d71d8b897455fc4fb9281f3b791ad3
Morgan Sell <MorganPSell@gmail.com>
2020-09-21 16:02:49 -0700

    chore(): Pre-processing data and creating new features.

---
+++ utils.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_null_val_heatmap(df, plot_title):
+    '''
+    Identifies null values within a dataframe.
+    
+    Args:
+        df (dataframe) : Dataset to evaluate null values.
+        plot_title(str) : Title of heatmap.
+    
+    Return
+        None
+    
+    '''
+    
+    plt.figure(figsize=(25, 10))
+    sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+    plt.title(plot_title, fontsize=26, fontweight='bold')
+    plt.tight_layout();
+    
+    
+def convert_price_to_float(price):
+    '''
+    Transforms price, which may be a string object, to a float object.
+    
+    Args:
+        price (str) : Price is expected to start w/ a "$".
+    
+    Returns:
+        adj_price (float) : Float object to be used for calculations/modelinng.
+    
+    '''
+
+    if price == '-':
+        adj_price = np.NaN
+        
+    elif type(price) == float:
+        adj_price = price
+        
+    elif type(price) == str:
+        tmp = price.replace('$', '')
+        adj_price = tmp.replace(',', '')
+        adj_price = float(adj_price)
+    
+    return adj_price
+
+def stats_summary(arr):
+    '''
+    Basic stats summary.
+    
+    Args:
+        arr (arr) : Array of numeric values.
+    
+    Returns:
+        None
+    
+    '''
+    print('STATS SUMMARY:')
+    print('Mean: ', arr.mean())
+    print('Median: ', arr.median())
+    print('Std. Dev.: ', arr.std())
+    print('Max value: ', arr.max())
+    print('Min value: ', arr.min())
+    
