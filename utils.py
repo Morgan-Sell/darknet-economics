1cfc63881e6084ff69121f38518daa610db15fec
Morgan Sell <MorganPSell@gmail.com>
2020-09-23 10:18:22 -0700

    chore(): Created a semi-unique identifier for products.

--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ def replace_empty_cell_with_nan(text):
     
     return revised_text
 
-def replace_split_text(text, re_chars, replacement):
+def clean_and_split_text(text, re_chars, replacement):
     '''
     Remove unecessary characters and transformed text into a list. 
     
@@ -102,8 +102,10 @@ def replace_split_text(text, re_chars, replacement):
         revised_text (arr) : Cleaned text in a list.
     
     '''
-    # regex_conversion = 'r'+
-    revised_text = re.sub(re_chars, replacement, str(text))
+    
+    revised_text = re.sub(r'\t', '', str(text))
+    revised_text = re.sub(r'\n', '', str(revised_text))
+    revised_text = re.sub(re_chars, replacement, revised_text)
     revised_text = re.split('\s+', revised_text)
     return revised_text[1:]
 
@@ -125,4 +127,44 @@ def aggregate_categories_to_master_groups(category):
     otc_keywords = ['Opioids', 'Prescription', 'Benzos', 'Opiods']
     fraud_security_goods_keywords = ['Fraud', 'Security']
     security_services_keywords = ['Hacking']
+
     
+    
+def create_product_id(seq, num_ints, year):
+    '''
+    Creates a product id by merging the initial sequence number or product id with the year the product was offered.
+    Adds zeros to the front of the "seq" value to ensure number of integers.
+    
+    Args:
+        seq (int) : A sequence of numbers.
+        num_int (int) : number of integer in the product id excluding the year.
+        year (int) : Four digits.
+        
+    Return:
+        product_id (str) : Identifier for the product. Format is "yy-nnnnnn". Number of digits ("n") can vary.
+    
+    '''
+    
+    seq_str = str(int(seq)).zfill(num_ints)
+    year_str = str(year)[2:]
+    product_id = year_str + '-' + seq_str
+    return product_id
+
+def select_non_null_value(x, y):
+    '''
+    Returns the non-null value. 
+    If neither value is null, returns x.
+    
+    Args:
+        x (float) : Float or
+        y (float) : number of integer in the product id excluding the year.
+         
+    Return:
+        res : Non-null value
+    '''
+    
+    if not np.isnan(x):
+        res = x
+    else:
+        res = y
+    return res
