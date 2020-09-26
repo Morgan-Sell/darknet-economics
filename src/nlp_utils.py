773d8522d9b45da6477ca45151828261a3550633
Morgan Sell <MorganPSell@gmail.com>
2020-09-26 15:33:18 -0700

    Analyzing LDA model results.

---
+++ src/nlp_utils.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re
+
+from bs4 import BeautifulSoup
+
+import nltk
+from nltk.corpus import stopwords
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+
+def clean_parse_text(text):
+    '''
+   
+    Args:
+        
+        
+    Return:
+        
+    '''
+    text = BeautifulSoup(text, 'html.parser').get_text()
+    text = re.sub(r'\n', ' ', text)
+    cleaned_text = re.sub(r'[^a-zA-Z0-9$!?.]', ' ', text)
+    return cleaned_text
+
+
+def clean_tokenize_lemmatize(text):
+    '''
+    Removes HTML tags and stopwords.
+    Converts words to its base using lemmatization.
+    
+    
+    Args:
+        comment (str) : Content of the author's post in Wall Street Market.
+        
+    Return:
+        lem_tokens (arr) : The cleaned, tokenized and lemmetized version of comment.
+        
+    '''
+    
+    cleaned_text = clean_parse_text(text)
+    
+    tokens = nltk.word_tokenize(cleaned_text)
+    tokens = [t for t in tokens if t not in stopwords.words('english')]
+    
+    wordnet_lemma = nltk.WordNetLemmatizer()
+    lem_tokens = [wordnet_lemma.lemmatize(t) for t in tokens]
+    
+    return lem_tokens
+
+
+def print_topics(model, count_vectorizer, n_top_words):
+    '''
+   
+    Args:
+    
+        
+    Return:
+    
+        
+    '''
+    
+    words = count_vectorizer.get_feature_names()
+    for topic_idx, topic in enumerate(model.components_):
+        top_words_arr = [words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
+        print("\nTopic #{}:".format(topic_idx))
+        print(" ".join(top_words_arr))
+
+        
+def convert_to_bow_and_fit_lda_model(text_dataset, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
+    '''
+    Convert text into a vector representation, i.e. Bag of Words.
+    
+    Args:
+        text_dataset(arr) : An array comprised of numerous texts.
+        max_feats (int) : Number of words to limit the bag-of-words.
+        freq-thresh (float) : Words that have a document frequency greater than the selected amount will be ignored.
+    Return:
+        text_transformed (arr) : Vectorized texted.
+    
+    '''
+    
+    vect = CountVectorizer(max_features=max_feats, max_df=freq_thresh)
+    text_transformed = vect.fit_transform(text_dataset)
+
+    lda = LatentDirichletAllocation(n_components=n_topics, learning_method=learning_method, max_iter=max_iter, random_state=random_state, n_jobs=-1)
+    doc_topics = lda.fit_transform(text_transformed)
+    
+    return doc_topics, lda, vect
---
+++ src/utils.py
@@ -0,0 +1,171 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re
+
+def plot_null_val_heatmap(df, plot_title, figsize):
+    '''
+    Identifies null values within a dataframe.
+    
+    Args:
+        df (dataframe) : Dataset to evaluate null values.
+        plot_title(str) : Title of heatmap.
+        figsize (tuple) : Plot dimensions
+    
+    Return:
+        None
+    
+    '''
+    
+    plt.figure(figsize=figsize)
+    sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+    plt.title(plot_title, fontsize=26, fontweight='bold')
+    plt.tight_layout();
+    
+    
+def convert_price_to_float(price):
+    '''
+    Transforms price, which may be a string object, to a float object.
+    
+    Args:
+        price (str) : Price is expected to start w/ a "$".
+    
+    Returns:
+        adj_price (float) : Float object to be used for calculations/modelinng.
+    
+    '''
+
+    if price == '-':
+        adj_price = np.NaN
+        
+    elif type(price) == float:
+        adj_price = price
+        
+    elif type(price) == str:
+        tmp = price.replace('$', '')
+        adj_price = tmp.replace(',', '')
+        adj_price = float(adj_price)
+    
+    return adj_price
+
+
+def stats_summary(arr):
+    '''
+    Basic stats summary.
+    
+    Args:
+        arr (arr) : Array of numeric values.
+    
+    Returns:
+        None
+    
+    '''
+    print('STATS SUMMARY:')
+    print('Mean: ', arr.mean())
+    print('Median: ', arr.median())
+    print('Std. Dev.: ', arr.std())
+    print('Max value: ', arr.max())
+    print('Min value: ', arr.min())
+    print('# of Obs.: ', len(arr))
+
+def replace_empty_cell_with_nan(text):
+    '''
+    Use with a dataframe column to replace an empty cell with dash with NaN.
+    
+    Args:
+        text (str) : String of characters.
+        
+    Return:
+        revised_text (str) : If cell is empty then returns NaN; otherwise, returns intial text.
+    
+    '''
+    if text == '':
+        revised_text = np.NaN
+   
+    else:
+        revised_text = text
+    
+    return revised_text
+
+def clean_and_split_text(text, re_chars, replacement):
+    '''
+    Remove unecessary characters and transformed text into a list. 
+    
+    Args:
+        text (str) : String of characters.
+        re_chars (str) : Input in regex format.
+        replacement (str) : String used to replace
+        
+    Return:
+        revised_text (arr) : Cleaned text in a list.
+    
+    '''
+    
+    revised_text = re.sub(r'\t', '', str(text))
+    revised_text = re.sub(r'\n', '', str(revised_text))
+    revised_text = re.sub(re_chars, replacement, revised_text)
+    revised_text = re.split('\s+', revised_text)
+    return revised_text
+
+def aggregate_categories_to_master_groups(category):
+    '''
+    Summarizes products into master products
+    
+    Args:
+        text (str) : String of characters.
+        re_chars (str) : Input in regex format.
+        replacement (str) : String used to replace
+        
+    Return:
+        revised_text (arr) : Cleaned text in a list.
+    
+    '''
+    illicit_keywords = ['Psychedelics', 'Ecstacy']
+    cannibis_keywords = ['Cannibis',]
+    otc_keywords = ['Opioids', 'Prescription', 'Benzos', 'Opiods']
+    fraud_security_goods_keywords = ['Fraud', 'Security']
+    security_services_keywords = ['Hacking']
+
+    
+    
+def create_product_id(seq, num_ints, year):
+    '''
+    Creates a product id by merging the initial sequence number or product id with the year the product was offered.
+    Adds zeros to the front of the "seq" value to ensure number of integers.
+    
+    Args:
+        seq (int) : A sequence of numbers.
+        num_int (int) : number of integer in the product id excluding the year.
+        year (int) : Four digits.
+        
+    Return:
+        product_id (str) : Identifier for the product. Format is "yy-nnnnnn". Number of digits ("n") can vary.
+    
+    '''
+    
+    seq_str = str(int(seq)).zfill(num_ints)
+    year_str = str(year)[2:]
+    product_id = year_str + '-' + seq_str
+    return product_id
+
+def select_non_null_value(x, y):
+    '''
+    Returns the non-null value. 
+    If neither value is null, returns x.
+    
+    Args:
+        x (float) : Float or
+        y (float) : number of integer in the product id excluding the year.
+         
+    Return:
+        res : Non-null value
+    '''
+    
+    if not np.isnan(x):
+        res = x
+    else:
+        res = y
+    return res
