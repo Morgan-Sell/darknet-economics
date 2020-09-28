29e30961b481e5a0f09c44be2bda0e81e0b86fa1
Morgan Sell <MorganPSell@gmail.com>
2020-09-27 20:25:48 -0700

    chore(): improving LDA model.

--- src/nlp_utils.py
+++ src/nlp_utils.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 
@@ -26,7 +26,7 @@ def clean_parse_text(text):
     '''
     text = BeautifulSoup(text, 'html.parser').get_text()
     text = re.sub(r'\n', ' ', text)
-    cleaned_text = re.sub(r'[^a-zA-Z0-9$!?.]', ' ', text)
+    cleaned_text = re.sub(r'[^a-zA-Z0-9]', ' ', text)
     return cleaned_text
 
 
@@ -47,6 +47,7 @@ def clean_tokenize_lemmatize(text):
     cleaned_text = clean_parse_text(text)
     
     tokens = nltk.word_tokenize(cleaned_text)
+    tokens = [t.lower() for t in tokens]
     tokens = [t for t in tokens if t not in stopwords.words('english')]
     
     wordnet_lemma = nltk.WordNetLemmatizer()
@@ -73,12 +74,12 @@ def print_topics(model, count_vectorizer, n_top_words):
         print(" ".join(top_words_arr))
 
         
-def convert_to_bow_and_fit_lda_model(text_dataset, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
+def convert_to_bow_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
     '''
     Convert text into a vector representation, i.e. Bag of Words.
     
     Args:
-        text_dataset(arr) : An array comprised of numerous texts.
+        docs_raw (arr) : An array comprised of numerous texts.
         max_feats (int) : Number of words to limit the bag-of-words.
         freq-thresh (float) : Words that have a document frequency greater than the selected amount will be ignored.
     Return:
@@ -87,13 +88,34 @@ def convert_to_bow_and_fit_lda_model(text_dataset, max_feats, freq_thresh, n_top
     '''
     
     vect = CountVectorizer(max_features=max_feats, max_df=freq_thresh)
-    text_transformed = vect.fit_transform(text_dataset)
+    docs_transformed = vect.fit_transform(docs_raw)
 
     lda = LatentDirichletAllocation(n_components=n_topics, learning_method=learning_method, max_iter=max_iter, random_state=random_state, n_jobs=-1)
-    doc_topics = lda.fit_transform(text_transformed)
+    doc_topics = lda.fit_transform(docs_transformed)
     
     return doc_topics, lda, vect
 
+
+def calculate_tfidf_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
+    '''
+    Convert text into a vector representation, i.e. Bag of Words.
+    
+    Args:
+        docs_raw (arr) : An array comprised of numerous texts.
+        max_feats (int) : Number of words to limit the bag-of-words.
+        freq-thresh (float) : Words that have a document frequency greater than the selected amount will be ignored.
+    Return:
+        text_transformed (arr) : Vectorized texted.
+    
+    '''
+    
+    vect = TfidfVectorizer(max_features=max_feats, max_df=freq_thresh)
+    docs_transformed = vect.fit_transform(docs_raw)
+
+    lda = LatentDirichletAllocation(n_components=n_topics, learning_method=learning_method, max_iter=max_iter, random_state=random_state, n_jobs=-1)
+    doc_topics = lda.fit_transform(docs_transformed)
+    
+    return doc_topics, lda, vect
 
 def print_topics(model, count_vectorizer, n_top_words):
     '''
@@ -108,3 +130,5 @@ def print_topics(model, count_vectorizer, n_top_words):
         top_words_arr = [words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
         print("\nTopic #{}:".format(topic_idx))
         print(" ".join(top_words_arr))
+        
+        
