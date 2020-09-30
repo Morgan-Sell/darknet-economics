8fe55c5580524f053efc681b50ec345a88c8ce47
Morgan Sell <MorganPSell@gmail.com>
2020-09-29 21:14:07 -0700

    Finalized lda_wallstreet.ipynb (for now). Need to review wallstreet_feat_eng_eda.ipynb. Determine best methodology in cleaning text.

--- src/lda_utils.py
+++ src/lda_utils.py
@@ -15,63 +15,6 @@ from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 
-def clean_parse_text(text):
-    '''
-   
-    Args:
-        
-        
-    Return:
-        
-    '''
-    text = BeautifulSoup(text, 'html.parser').get_text().lower()
-    text = re.sub(r'\n', ' ', text)
-    text = re.sub(r"\[?!.,/']*", '', text)
-    cleaned_text = re.sub(r'\w*\d\w*', '', text)
-    return cleaned_text
-
-
-def tokenize_lemmatize(text):
-    '''
-    Removes HTML tags and stopwords.
-    Converts words to its base using lemmatization.
-    
-    
-    Args:
-        comment (str) : Content of the author's post in Wall Street Market.
-        
-    Return:
-        lem_tokens (arr) : The cleaned, tokenized and lemmetized version of comment.
-        
-    '''
-    
-    tokens = nltk.word_tokenize(text)
-    tokens = [t for t in tokens if t not in stopwords.words('english')]
-    
-    wordnet_lemma = nltk.WordNetLemmatizer()
-    lem_tokens = [wordnet_lemma.lemmatize(t) for t in tokens]
-    
-    return lem_tokens
-
-
-def print_topics(model, count_vectorizer, n_top_words):
-    '''
-   
-    Args:
-    
-        
-    Return:
-    
-        
-    '''
-    
-    words = count_vectorizer.get_feature_names()
-    for topic_idx, topic in enumerate(model.components_):
-        top_words_arr = [words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
-        print("\nTopic #{}:".format(topic_idx))
-        print(" ".join(top_words_arr))
-
-        
 def convert_to_bow_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
     '''
     Convert text into a vector representation, i.e. Bag of Words.
@@ -92,6 +35,8 @@ def convert_to_bow_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics,
     lda_output = lda_model.fit_transform(docs_vectorized)
     
     return lda_output, lda_model, docs_vectorized, vectorizer
+
+
 
 
 def calculate_tfidf_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics, learning_method, max_iter, random_state=3):
@@ -115,18 +60,29 @@ def calculate_tfidf_and_fit_lda_model(docs_raw, max_feats, freq_thresh, n_topics
     
     return lda_output, lda_model, docs_vectorized, vectorizer
 
+
+
 def print_topics(model, count_vectorizer, n_top_words):
     '''
-    
+   
     Args:
+    
         
     Return:
+    
         
     '''
+    
     words = count_vectorizer.get_feature_names()
     for topic_idx, topic in enumerate(model.components_):
         top_words_arr = [words[i] for i in topic.argsort()[:-n_top_words - 1:-1]]
         print("\nTopic #{}:".format(topic_idx))
         print(" ".join(top_words_arr))
+
         
+
+
+
+
+
         
